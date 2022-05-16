e41bafef1bf5611c3da4632fe60b2322e8470ddc
Adduriel <78422565+Adduriel@users.noreply.github.com>
2022-05-15 20:03:08 -0500

    affine.py

--- main (1).py
+++ main (1).py
@@ -1,47 +1,44 @@
-def fangshe(p,k1,k2):
-	c=""
-	for i in range(len(p)):
-		temp=chr((((ord(p[i])-ord('a'))*k1+k2)%26)+ord('a'))
-		c+=temp
-	return c
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-def gcd(x,y):
-	if x>y:
-		temp=y
-	else:
-		temp=x
-	for i in range(1,temp+1):
-		if((x%i==0) and (y%i==0)):
-			s=i
-	return s
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/cesargomez/Desktop/blogyoutube/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-def decode(a,b,str1):
-	c1=""
-	if(gcd(a,26)==1):
-		for i in range(26):
-			if((a*i)%26==1):
-				k1_re=i
-				break
-		for i in range(len(str1)):
-			temp=chr((((ord(str1[i])-ord('a'))-b)*k1_re)%26+ord('a'))
-			c1+=temp
-		return c1
-    
-def main():
-	print 8*"*"+" EncryptInput "+8*"*"
-	p=raw_input("Please enter the plain:")
-	k1=input("Please enter k1:")
-	k2=input("Please enter k2:")
-	print 8*"*"+" Encryption "+8*"*"
-	c=fangshe(p,k1,k2)
-	print ("Cipher:"+c)
-	print "\n"+8*"*"+" DecryptInput "+8*"*"
-	C=raw_input("Please enter the Cipher:")
-	key1=input("Please enter k1:")
-	key2=input("Please enter k2:")
-	print 8*"*"+" Decryption "+8*"*"
-	plain=decode(key1,key2,C)
-	print "Plain:"+plain
-    
-if __name__ == '__main__':
-	main()
+class Post(db.Model):
+	__tablename__ = "posts"
+	id = db.Column(db.Integer, primary_key=True)
+	titulo = db.Column(db.String, nullable=False)
+	fecha = db.Column(db.DateTime, default=datetime.now)
+	texto = db.Column(db.String, nullable=False)
+
+@app.route("/")
+def inicio():
+	posts = Post.query.order_by(Post.fecha.desc()).all() 
+	return render_template("inicio.html", posts=posts)
+
+@app.route("/agregar")
+def agregar():
+	return render_template("agregar.html")
+
+@app.route("/crear", methods=["POST"])
+def crear_post():
+	titulo = request.form.get("titulo")
+	texto = request.form.get("texto")
+	post = Post(titulo=titulo, texto=texto)
+	db.session.add(post)
+	db.session.commit()
+	return redirect("/")
+
+@app.route("/borrar", methods=["POST"])
+def borrar():
+	post_id = request.form.get("post_id")
+	post = db.session.query(Post).filter(Post.id==post_id).first()
+	db.session.delete(post)
+	db.session.commit()
+	return redirect("/")
+
+if __name__ == "__main__":
+	app.run(debug=True)
