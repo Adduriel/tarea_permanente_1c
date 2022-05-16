32b3a027888776a32a689c77854521f58b80d07e
Adduriel <78422565+Adduriel@users.noreply.github.com>
2022-05-15 22:28:55 -0500

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,44 +1,56 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from utils import meta, euclidean, extendedEuclidean, inverse
+    
+def encrypt(a, b, text):
+  result = ""
+  
+  for i in text:
+    
+    n = 0
+    for j in meta:
+      if i==j:
+        break
+      n += 1
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/cesargomez/Desktop/blogyoutube/blog.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+    idx = (a * n + b ) % len(meta)
+    result += meta[idx]
+  
+  return result
 
-class Post(db.Model):
-	__tablename__ = "posts"
-	id = db.Column(db.Integer, primary_key=True)
-	titulo = db.Column(db.String, nullable=False)
-	fecha = db.Column(db.DateTime, default=datetime.now)
-	texto = db.Column(db.String, nullable=False)
+def decrypt(a, b, text):
+  result=""
+  
+  for i in text:
+    
+    n = 0
+    for j in meta:
+      if i==j:
+        break
+      n += 1 
+    idx = (inverse(a,len(meta)) * (n - b)) % len(meta)
+    result += meta[idx]
+  
+  return result
 
-@app.route("/")
-def inicio():
-	posts = Post.query.order_by(Post.fecha.desc()).all() 
-	return render_template("inicio.html", posts=posts)
+'''
+    CODE EXECUTION
+'''
 
-@app.route("/agregar")
-def agregar():
-	return render_template("agregar.html")
+def run():
+    a=2
+    n=4
 
-@app.route("/crear", methods=["POST"])
-def crear_post():
-	titulo = request.form.get("titulo")
-	texto = request.form.get("texto")
-	post = Post(titulo=titulo, texto=texto)
-	db.session.add(post)
-	db.session.commit()
-	return redirect("/")
+    key = 4, 7
 
-@app.route("/borrar", methods=["POST"])
-def borrar():
-	post_id = request.form.get("post_id")
-	post = db.session.query(Post).filter(Post.id==post_id).first()
-	db.session.delete(post)
-	db.session.commit()
-	return redirect("/")
+    inverse_result = inverse(a, n)
 
-if __name__ == "__main__":
-	app.run(debug=True)
+    text="ELEMENTALMIQUERIDOWATSON"
+    text_result = encrypt(*key,text)
+
+    cipher_text="WXWBWFGHXBMUKWYMSNRHGCNF"
+    cipher_text_result=decrypt(*key,cipher_text)
+
+    print(inverse_result)
+    print(text_result)
+    print(cipher_text_result)
+
+run()
